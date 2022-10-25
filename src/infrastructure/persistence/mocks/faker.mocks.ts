6a39f59f0cd12fae1db3791076adbaa3c2f861ed
EuLuiiz <victorluiz011@gmail.com>
2022-10-25 15:55:21 -0300

    FIX: Implementando imagens para dados Mock (faker)

--- src/infrastructure/persistence/mocks/faker.mocks.ts
+++ src/infrastructure/persistence/mocks/faker.mocks.ts
@@ -5,17 +5,19 @@ import { faker } from '@faker-js/faker';
 export default class fakerMocks implements IMocks {
     getUsers(): IUserEntity[] {
         const users: IUserEntity[] = [];
-        let nameG, emailG, apartmentG, passwordG;
+        let nameG, emailG, apartmentG, passwordG, imagelinkG;
         Array.from({ length: 20 }).forEach(() => {
             nameG = faker.name.fullName();
             emailG = faker.helpers.unique(faker.internet.email, [nameG]).toLocaleLowerCase();
             apartmentG = Number(faker.random.numeric(2));
-            passwordG = `admin${apartmentG}`
+            passwordG = `admin${apartmentG}`;
+            imagelinkG = faker.image.cats(640, 480, true)
             users.push({
                 name: nameG,
                 email: emailG,
                 apartment: apartmentG,
-                password: passwordG
+                password: passwordG,
+                imagelink: imagelinkG
             })
         })
         return users;
