3039ad3b5a214b39d029539130b536ce73ead638
EuLuiiz <victorluiz011@gmail.com>
2022-10-26 06:08:36 -0300

    Melhorando conteudo do token

--- src/adapters/helpers/token.login.generator.ts
+++ src/adapters/helpers/token.login.generator.ts
@@ -2,17 +2,13 @@ import jwt from 'jsonwebtoken';
 import { secretKey } from '../../infrastructure/config/token/secret';
 import listUsersUsecase from '../../domain/usecases/users/list.users.usecase';
 
-export default async function (user: { iduser: number, name: string, email: string, apartment: number, imagelink: string }) {
+export default async function (request: { email: string }) {
     const users = await listUsersUsecase.execute();
-    const repeat = users?.find(dados => dados.email === user.email);
+    const repeat = users?.find(user => user.email === request.email);
     const token = jwt.sign({
-        iduser: repeat!.iduser,
-        name: repeat!.name,
-        email: repeat!.email,
-        apartment: repeat!.apartment,
-        imagelink: repeat!.imagelink
+        iduser: repeat!.iduser
     }, secretKey, {
-        expiresIn: '7 days'
+        expiresIn: '2 days'
     })
     return token;
 }
